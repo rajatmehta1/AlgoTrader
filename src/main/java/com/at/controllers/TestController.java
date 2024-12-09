e4ea7ab1c37cd4143a400be4d0c6092df0cecbfd
rajat <rmehta81@gatech.edu>
2024-12-09 12:42:53 -0500

    first checkin

--- src/main/java/com/at/controllers/TestController.java
+++ src/main/java/com/at/controllers/TestController.java
@@ -1,15 +1,27 @@
 package com.at.controllers;
 
+import com.at.entity.Test;
+import com.at.repository.TestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController {
 
+    @Autowired
+    private TestRepository testRepository;
 
     @GetMapping("/trader/hello")
     public String helloTrader() {
         return "hellow trader";
+    }
+
+    @GetMapping("/trader/tests")
+    public List<Test> tests() {
+        return testRepository.findAll();
     }
 
 }
--- src/test/java/com/at/AlgoTraderApplicationTests.java
+++ src/test/java/com/at/AlgoTraderApplicationTests.java
@@ -3,11 +3,11 @@ package com.at;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AlgoTraderApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
